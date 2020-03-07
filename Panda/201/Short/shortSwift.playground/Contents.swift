96900b73e27f270a82d57faf3f27fb8a4e4fc60c
TRA <nayhan@trabilisim.com>
2020-03-07 17:29:29 +0300

    Structure Completed

---
+++ Panda/201/Short/shortSwift.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+import Foundation
+ 
+let fmt = NumberFormatter()
+fmt.numberStyle = .DecimalStyle
+fmt.stringFromNumber(2358000)
+print("fmtd",fmt)
+
+
---
+++ Panda/Mackup Queen/Mackup Queen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mackup Queen
+//
+//  Created by TRA on 22.01.2020.
+//  Copyright © 2020 tra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
