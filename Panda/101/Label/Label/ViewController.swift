69e5231295e8970eff4aff7ffe372cbf48c3b1c9
TRA <nayhan@trabilisim.com>
2020-06-09 10:20:15 +0300

    fx

--- Panda/101/Label/Label/ViewController.swift
+++ Panda/101/Label/Label/ViewController.swift
@@ -17,15 +17,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textLabel.text = "Metin"
-        textLabel.textColor = UIColor.black
-        textLabel.numberOfLines = 2
-//        textLabel.adjustsFontSizeToFitWidth = true
-        textLabel.font = UIFont.init(name: "Roboto", size: 10)
-        textLabel.attributedText = NSAttributedString.init(string: "Değiştirilmesi daha kolay metin formatı olarak kullanılır.", attributes: .init())
-        textLabel.frame  = CGRect(x: (view.frame.width -  textLabel.frame.width)/2, y: view.frame.midY, width: textLabel.frame.width , height: 20)
-        
-         
         
     }
     func addBottomBorder(_ uiView:UIView ,  borderWidth: CGFloat) ->UIView {
