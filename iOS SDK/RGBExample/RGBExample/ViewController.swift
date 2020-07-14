dcc9e73fab55cc84e9f0828ec482096d1b40c970
nayhan <nayhan@trabilisim.com>
2020-07-14 11:17:39 +0300

    fx

---
+++ iOS SDK/RGBExample/RGBExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RGBExample
+//
+//  Created by nayhan on 14.07.2020.
+//  Copyright © 2020 hrdijital. All rights reserved.
+//
+
+import UIKit
+import RGB
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor =
+         
+    }
+}
+
