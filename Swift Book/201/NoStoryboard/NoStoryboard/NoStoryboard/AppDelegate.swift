456e6440b396655fabfce20feadb9fe031a3e439
numan <nayhan@trabilisim.com>
2020-07-17 15:34:41 +0300

    fx

---
+++ Swift Book/201/NoStoryboard/NoStoryboard/NoStoryboard/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  NoStoryboard
+//
+//  Created by nayhan on 17.07.2020.
+//  Copyright © 2020 hrdijital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window : UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: Menu())
+        window?.makeKeyAndVisible()
+        return true
+    }
+ 
+
+
+}
+
---
+++ Swift Book/201/NoStoryboard/NoStoryboard/NoStoryboard/Menu.swift
@@ -0,0 +1,18 @@
+//
+//  Menu.swift
+//  NoStoryboard
+//
+//  Created by nayhan on 17.07.2020.
+//  Copyright © 2020 hrdijital. All rights reserved.
+//
+
+import UIKit
+
+class Menu: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    } 
+}
