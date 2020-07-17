177a9755ef7809c0fc7e153edbbdedf9429e2c8f
numan <nayhan@trabilisim.com>
2020-07-17 15:49:02 +0300

    add button

--- Swift Book/201/NoStoryboard/NoStoryboard/NoStoryboard/Menu.swift
+++ Swift Book/201/NoStoryboard/NoStoryboard/NoStoryboard/Menu.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class Menu: UIViewController {
 
+    lazy var login  : UIButton = {
+       
+        let btn =  UIButton()
+        btn.setTitle("Giriş Yap", for: .normal)
+        btn.layer.borderColor = UIColor.red.cgColor
+        btn.layer.borderWidth = 0.2
+        btn.backgroundColor = UIColor.white
+        btn.setTitleColor(.yellow, for: .normal)
+        return btn
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = .yellow
-    } 
+          
+         
+         view.addSubview(login)
+         login.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 250, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height:50)
+        
+    }
+    override func viewDidAppear(_ animated: Bool) {
+         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.backgroundColor  = UIColor.clear
+        navigationController?.navigationBar.shadowImage = UIImage()
+        navigationController!.navigationBar.topItem?.title = ""
+    }
 }
